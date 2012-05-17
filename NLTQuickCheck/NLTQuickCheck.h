152cb01b6a35fdbc60b990e0fafc4b4efd41b94f
KAZUMA Ukyo <yaakaito@gmail.com>
2012-05-18 04:00:00 +0900

    setup

---
+++ NLTQuickCheck/NLTQuickCheck.h
@@ -0,0 +1,13 @@
+//
+//  NLTQuickCheck.h
+//  NLTQuickCheck
+//
+//  Created by KAZUMA Ukyo on 12/05/18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLTQuickCheck : NSObject
+
+@end
---
+++ NLTQuickCheck/NLTQuickCheck.m
@@ -0,0 +1,13 @@
+//
+//  NLTQuickCheck.m
+//  NLTQuickCheck
+//
+//  Created by KAZUMA Ukyo on 12/05/18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "NLTQuickCheck.h"
+
+@implementation NLTQuickCheck
+
+@end

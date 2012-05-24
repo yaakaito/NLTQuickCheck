607ab17d468f1e150a76e582beb1d90378005a70
KAZUMA Ukyo <yaakaito@gmail.com>
2012-05-25 03:28:03 +0900

    chooseのサポートメソッドのテスト

--- NLTQuickCheckTests/NLTQGenCombinatorSpec.m
+++ NLTQuickCheckTests/NLTQGenCombinatorSpec.m
@@ -8,6 +8,10 @@
 #import "Kiwi.h"
 #import "NLTQGen.h"
 
+@interface NLTQGen(Privates)
++ (NSArray*)numbersArrayWithLow:(NSInteger)low high:(NSInteger)high;
+@end
+
 SPEC_BEGIN(NLTQGenCombinatorSpec)
 
 describe(@"Gen(erator)", ^{
@@ -30,6 +34,29 @@ describe(@"Gen(erator)", ^{
     
     context(@"class method ``elementsGenWithArray:`` ", ^{
         
+        context(@"support method ``numbersArrayWithLow:high`` ", ^{
+           
+            context(@"when call low 20 and high 30", ^{
+                __block NSArray *array;
+                
+                beforeEach(^{
+                    array = [NLTQGen numbersArrayWithLow:20 high:30];
+                });
+                
+                it(@"has 11 numbers", ^{
+                    [[[array should] have:11] count];
+                });
+                
+                it(@"first object 20", ^{
+                    [[theValue([[array objectAtIndex:0] intValue]) should] equal:theValue(20)];
+                });
+                
+                it(@"lastest object 30", ^{
+                    [[theValue([[array lastObject] intValue]) should] equal:theValue(30)];
+                });
+            });
+        }); 
+
         context(@"call with [1,2,3]", ^{
             __block NLTQGen *gen;
             

e3ba673d123f9b7cf361093c633e0b44dcc4491d
KAZUMA Ukyo <yaakaito@gmail.com>
2012-05-25 02:56:51 +0900

    いややっぱこれであってる

--- NLTQuickCheckTests/NLTQGenSpec.m
+++ NLTQuickCheckTests/NLTQGenSpec.m
@@ -12,6 +12,37 @@ SPEC_BEGIN(NLTQGenSpec)
 
 describe(@"Gen(erator)", ^{
     __block NLTQGen *gen;
+    context(@"create with fixation generater and return 'string'", ^{
+        beforeEach(^{
+            gen = [NLTQGen genWithGenerateBlock:^id(double progress) {
+                return @"string";
+            }];
+        });
+        
+        it(@"at progress 0.1 , the value is 'string'(fixed)", ^{
+            [[[gen valueWithProgress:0.1] should] equal:@"string"];
+        });
+        
+        it(@"at progress 0.2 , the value is 'string'(fixed)", ^{
+            [[[gen valueWithProgress:0.2] should] equal:@"string"];
+        });
+    });
+    
+    context(@"create with rebound generater and return progress", ^{
+        beforeEach(^{
+            gen = [NLTQGen genWithGenerateBlock:^id(double progress) {
+                return [NSNumber numberWithDouble:progress];
+            }];
+        });
+        
+        it(@"at progress 0.0 , the value is 0.0 NSNumber object", ^{
+            [[theValue([[gen valueWithProgress:0.0] doubleValue]) should] equal:theValue(0.0)];
+        });
+        
+        it(@"at progress 0.5 , the value is 0.5 NSNumber object", ^{
+            [[theValue([[gen valueWithProgress:0.5] doubleValue]) should] equal:theValue(0.5)];
+        });
+    });
 });
 
 SPEC_END

38e5b1237902459e26ff824bd95a6674fe90891f
KAZUMA Ukyo <yaakaito@gmail.com>
2012-05-24 12:26:37 +0900

    固定のgenerateBlockを受け取ったときの処理1

--- NLTQuickCheck/NLTQGen.m
+++ NLTQuickCheck/NLTQGen.m
@@ -8,6 +8,27 @@
 
 #import "NLTQGen.h"
 
-@implementation NLTQGen
+@implementation NLTQGen {
+    
+    __generateBlock _block;
+}
 
+- (id)initWithGenerateBlock:(__generateBlock)block {
+    
+    self = [super init];
+    if(self) {
+        _block = block;
+    }
+    return self;
+}
+
++ (id)genWithGenerateBlock:(__generateBlock)block {
+    
+    return [[self alloc] initWithGenerateBlock:block];
+}
+
+- (id)valueWithProgress:(double)progress {
+    
+    return _block(progress);
+}
 @end

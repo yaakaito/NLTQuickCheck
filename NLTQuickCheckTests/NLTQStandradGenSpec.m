22807bc161c90388c910a4db4ce0c5b5df3c9044
KAZUMA Ukyo <yaakaito@gmail.com>
2012-05-30 01:40:14 +0900

    StandardGenSpecださかったので直した

--- NLTQuickCheckTests/NLTQStandradGenSpec.m
+++ NLTQuickCheckTests/NLTQStandradGenSpec.m
@@ -13,27 +13,26 @@ SPEC_BEGIN(NLTQStandradGenSpec)
 describe(@"Standard Gen(erator)", ^{
     __block NLTQStandardGen *gen;
     
-    context(@"create new with minimum = 10 and maximum = 20", ^{
+    context(@"with minimum = 10 and maximum = 20", ^{
         
         beforeEach(^{
             gen = [NLTQStandardGen standardGenWithMinimumSeed:10 maximumSeed:20];
         });
         
-        it(@"minimum is 10", ^{
+        it(@"when minimum is 10", ^{
             [[theValue(gen.minimumSeed) should] equal:theValue(10)];
         });
         
-        it(@"maximum is 20", ^{
+        it(@"when maximum is 20", ^{
             [[theValue(gen.maximumSeed) should] equal:theValue(20)];
         });
         
-        it(@"current generated value in 10~20", ^{
-            [[theValue(gen.currentGeneratedValue) should] beGreaterThanOrEqualTo:theValue(10)];
-            [[theValue(gen.currentGeneratedValue) should] beLessThanOrEqualTo:theValue(20)];
+        it(@"when wcurrent generated value in 10~20", ^{
+            [[theValue(gen.currentGeneratedValue) should] beBetween:theValue(10) and:theValue(20)];
         });
     });
     
-    context(@"create new with minimum = 20 and maximum = 30", ^{
+    context(@"with minimum = 20 and maximum = 30", ^{
         
         beforeEach(^{
             gen = [NLTQStandardGen standardGenWithMinimumSeed:20 maximumSeed:30];
@@ -48,8 +47,7 @@ describe(@"Standard Gen(erator)", ^{
         });
         
         it(@"current generated value in 20~30", ^{
-            [[theValue(gen.currentGeneratedValue) should] beGreaterThanOrEqualTo:theValue(20)];
-            [[theValue(gen.currentGeneratedValue) should] beLessThanOrEqualTo:theValue(30)];
+            [[theValue(gen.currentGeneratedValue) should] beBetween:theValue(20) and:theValue(30)];
         });
     });
 });

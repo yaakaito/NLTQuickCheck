89261dbf1e21ee42630916481d671647a62d61d5
KAZUMA Ukyo <yaakaito@gmail.com>
2012-06-05 21:34:19 +0900

    Runnerのテストこんな感じかな

--- NLTQuickCheckTests/NLTQTestRunnerSpec.m
+++ NLTQuickCheckTests/NLTQTestRunnerSpec.m
@@ -62,7 +62,114 @@ describe(@"Test Runner", ^{
         
     });
     
+    context(@"failure at 3rd , 4th and 5th test case", ^{
+        __block NLTQTestRunner *testRunner;
+        __block int runCounter;
+        beforeEach(^{
+            runCounter = 0;
+            NLTQTestCase *testCase = [NLTQTestCase blocksTestCaseWithBlocksArguments0:^BOOL{
+                runCounter++;
+                if(runCounter == 3 || runCounter == 4 || runCounter == 5) {
+                    return NO;
+                }
+                return YES;
+            }];
+            testRunner = [NLTQTestRunner testRunnerWithTestCase:testCase];
+        });
+        
+        context(@"and run 6 cases ", ^{
+            __block NSArray *reports;
+            beforeEach(^{
+                [testRunner setTestLength:6];
+                [testRunner runWithVerbose:NO reports:&reports];
+            });
+            
+            it(@"when should 6 executed", ^{
+                [[theValue(runCounter) should] equal:theValue(6)];
+            });
+            
+            it(@"when should has 6 reports", ^{
+                [[theValue([reports count]) should] equal:theValue(6)];
+            });
+            
+            it(@"when should 1st report and 2nd report are success", ^{
+                [[theValue([[reports objectAtIndex:0] success]) should] beYes];
+                [[theValue([[reports objectAtIndex:1] success]) should] beYes];
+            });
+            
+            it(@"when should 3rd report is failure (needsRetry=YES,retryCount=0,isException=NO)", ^{
+                [[theValue([[reports objectAtIndex:2] success]) should] beNo];
+                [[theValue([[reports objectAtIndex:2] isException]) should] beNo];
+                [[theValue([[reports objectAtIndex:2] needsRetry]) should] beYes];
+            });
+            
+            it(@"when should 4th report is failure (needsRetry=YES,retryCount=1,isException=NO)", ^{
+                [[theValue([[reports objectAtIndex:3] success]) should] beNo];
+                [[theValue([[reports objectAtIndex:3] isException]) should] beNo];
+                [[theValue([[reports objectAtIndex:3] needsRetry]) should] beYes];
+                [[theValue([[reports objectAtIndex:3] retryCounter]) should] equal:theValue(1)];
+            });
 
+            it(@"when should 5th report is failure (needsRetry=NO,retryCount=2,isException=NO)", ^{
+                [[theValue([[reports objectAtIndex:4] success]) should] beNo];
+                [[theValue([[reports objectAtIndex:4] isException]) should] beNo];
+                [[theValue([[reports objectAtIndex:4] needsRetry]) should] beNo];
+                [[theValue([[reports objectAtIndex:4] retryCounter]) should] equal:theValue(2)];
+            });
+            
+            it(@"when should 6th report is success", ^{
+                [[theValue([[reports objectAtIndex:5] success]) should] beYes];
+            });
+        });
+    });
+    
+    context(@"throws exception at 3rd test case", ^{
+        __block NLTQTestRunner *testRunner;
+        __block int runCounter;
+        beforeEach(^{
+            runCounter = 0;
+            NLTQTestCase *testCase = [NLTQTestCase blocksTestCaseWithBlocksArguments0:^BOOL{
+                runCounter++;
+                if(runCounter == 3) {
+                    @throw [NSException exceptionWithName:@"dummy exception" reason:@"test" userInfo:nil];
+                }
+                return YES;
+            }];
+            testRunner = [NLTQTestRunner testRunnerWithTestCase:testCase];
+        });
+        
+        context(@"and run 4 cases (retry case)", ^{
+            __block NSArray *reports;
+            beforeEach(^{
+                [testRunner setTestLength:4];
+                [testRunner runWithVerbose:NO reports:&reports];
+            });
+            
+            it(@"when should 4 executed", ^{
+                [[theValue(runCounter) should] equal:theValue(4)];
+            });
+            
+            it(@"when should has 4 reports", ^{
+                [[theValue([reports count]) should] equal:theValue(4)];
+            });
+            
+            it(@"when should 1st report and 2nd report are success", ^{
+                [[theValue([[reports objectAtIndex:0] success]) should] beYes];
+                [[theValue([[reports objectAtIndex:1] success]) should] beYes];
+            });
+            
+            it(@"when should 3rd report is failure (isException=YES, needsRetry=NO)", ^{
+                [[theValue([[reports objectAtIndex:2] success]) should] beNo];
+                [[theValue([[reports objectAtIndex:2] isException]) should] beYes];
+                [[theValue([[reports objectAtIndex:2] needsRetry]) should] beNo];
+            });
+            
+            it(@"when should 4th report is success", ^{
+                [[theValue([[reports objectAtIndex:3] success]) should] beYes];
+            });
+        });
+    });
+    
 });
 
 SPEC_END

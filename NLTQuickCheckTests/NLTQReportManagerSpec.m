b78f4de29ff945ff39ff29617e6e50856816c8ee
KAZUMA Ukyo <yaakaito@gmail.com>
2012-06-10 17:24:16 +0900

    addReportsFromArrayのテスト書いた

--- NLTQuickCheckTests/NLTQReportManagerSpec.m
+++ NLTQuickCheckTests/NLTQReportManagerSpec.m
@@ -87,6 +87,30 @@ describe(@"Report Manager", ^{
             [[theValue([reportManager.failureReports count]) should] beZero];
         });
     });
+    
+    context(@"add reports from array", ^{
+        beforeEach(^{
+            NLTQReport *success1 = [NLTQReport reportWithSuccess:YES needsRetry:NO retryCounter:0 isException:NO arguments:nil];
+            NLTQReport *success2 = [NLTQReport reportWithSuccess:YES needsRetry:NO retryCounter:0 isException:NO arguments:nil];
+            NLTQReport *failure1 = [NLTQReport reportWithSuccess:NO needsRetry:YES retryCounter:0 isException:NO arguments:nil];
+            NLTQReport *failure2 = [NLTQReport reportWithSuccess:NO needsRetry:YES retryCounter:1 isException:NO arguments:nil];
+            NLTQReport *failure3 = [NLTQReport reportWithSuccess:NO needsRetry:NO retryCounter:2 isException:NO arguments:nil];
+            NLTQReport *exception = [NLTQReport reportWithSuccess:NO needsRetry:NO retryCounter:0 isException:YES arguments:nil];
+            [reportManager addReportsFromArray:[NSArray arrayWithObjects:success1, success2, failure1, failure2, failure3, exception, nil]];
+        });
+        
+        it(@"should successReports has 2 reports objects", ^{
+            [[theValue([reportManager.successReports count]) should] equal:theValue(2)];
+        });
+        
+        it(@"should failureReports has 3 reports objects", ^{
+            [[theValue([reportManager.failureReports count]) should] equal:theValue(3)];
+        });
+        
+        it(@"should exceptionReports has 1 reports objects", ^{
+            [[theValue([reportManager.exceptionReports count]) should] equal:theValue(1)];
+        });
+    });
 });
 
 SPEC_END
